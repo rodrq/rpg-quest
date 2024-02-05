5a0a6b5262a2dc595a6d71258c495458a2dbc898
Rodri Rooney <rodrqq@gmail.com>
2024-02-05 15:49:15 -0300

    fix typos and local docker compose

--- api/routers/quest.py
+++ api/routers/quest.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from controllers.quest import QuestGenerationParams, create_quest_handler
 from sqlalchemy.orm import Session
-from db.init import engine  # Assuming you have an engine for your database
+from db.init import engine 
 from db.models import Quest 
 router = APIRouter()
 
@@ -21,5 +21,4 @@ async def get_quests(params: GetQuests):
     quests = session.query(Quest).filter(Quest.character_name == params.character_name).all()
 
   quests_dict = [quest.__dict__ for quest in quests]
-
-  return quests_dictx
+  return quests_dict
