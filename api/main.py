d60854c07f7e467fc8aceb16cfe8f25eae907612
rodrq <rodrqq@gmail.com>
2024-02-09 03:56:20 -0300

    added cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.routes import quest, character, auth
 from src.config.database import Base, engine
-
+from fastapi.middleware.cors import CORSMiddleware
 def get_app() -> FastAPI:
 
     app = FastAPI()
@@ -13,6 +13,14 @@ def get_app() -> FastAPI:
     app.include_router(quest.router)
     
     app.include_router(auth.router)
+    
+    app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
     
 
     return app
