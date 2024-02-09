872551072dbf8a430cac85e3792a388d230e5a7b
rodrq <rodrqq@gmail.com>
2024-02-08 21:47:26 -0300

    setup jwt auth

---
+++ api/src/auth/auth.py
@@ -0,0 +1,44 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from src.utils.query import get_character
+from src.utils.pw_hash import verify_password
+from datetime import datetime, timedelta
+from src.config.config import SECRET_KEY, ALGORITHM
+from typing import Annotated
+from src.models.schemas import TokenData
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
+
+
+def create_access_token(data: dict, expires_delta = timedelta(minutes=30)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_character(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        jwt_character_username: str = payload.get("sub")
+        if jwt_character_username is None:
+            raise credentials_exception
+        token_data = TokenData(username=jwt_character_username)
+    except JWTError:
+        raise credentials_exception
+    character = get_character(username=token_data.username)
+    if character is None:
+        raise credentials_exception
+    return character
+
+def authenticate_character(username: str, password: str):
+    character = get_character(username)
+    if not character or not verify_password(password, character.password):
+        return None
+    return character
