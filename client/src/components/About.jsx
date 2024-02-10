4a07367459c61225f172fc5fcd6a78cd7fab9f48
rodrq <rodrqq@gmail.com>
2024-02-09 23:19:13 -0300

    created about page, uninstalled useless packages

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,16 +1,24 @@
 // About.js
 import React from 'react';
-
+import Prompt from '../assets/prompt.png'
 const About = () => {
   return (
     <div className="container mx-auto mt-8 px-10 py-5 text-gray-600 text-xl">
       <h1 className="text-2xl font-bold mb-4">About the project.</h1>
-      <p>Given enough context, the Language Model can create amazing stuff. You could tell it all about the lore of the game, the places,
-        the animals and monsters of each place, your character coded capabilities and actions, and would end with a real-time generated
-        JSON requested by the videogame NPC you just talked to. Now, the tooling needed to make it work client-side, no clue.</p>
-      <p></p>
-        
-      <a href='' target="_blank" rel="noopener noreferrer"></a>
+      <p className='my-2'>Given enough context, the Language Model can create amazing stuff. You could tell it all about the lore of the game, the places,
+        the animals and monsters of each place, your character coded capabilities and actions; and would end with a real-time generated
+        JSON representing a quest that was requested by the videogame NPC you just talked to.</p>
+      <p className='my-2'>
+        This project just accepts 'class' and 'map' as context to save on infra costs, but still the results are pretty good.
+      </p>
+      <p>
+        Prompt used is:
+      </p>
+      <img className='my-3'src={Prompt}>
+      </img>
+      <p>Project made with FastAPI, React, and OpenAI API.</p>
+      <p>Check the whole project here: <a href='https://github.com/rodrq/rpg-quest' className='underline'target="_blank" rel="noopener noreferrer">https://github.com/rodrq/rpg-quest</a> </p>
+
     </div>
   );
 };
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import GithubIcon from '../assets/github.svg';
 import LinkedinIcon from '../assets/linkedin.svg';
-
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="container mx-auto mt-8 px-10 py-5 text-gray-600 text-xl">
-      <h2 className="text-3xl font-semibold mb-4">Welcome to RPG Quest Generator</h2>
+      <h1 className="text-3xl font-semibold mb-2">RPG Quest Generator</h1>
+      <h2 className="text-2xl">
+        Dynamic videogame quest generation using OpenAI's language model. 
+      </h2>
       <p className="py-1">
-        Test a basic version of what I believe is the future of MMORPGs. 
-      </p>
-      <p className="pt-1">
-        AI dynamic quest generation with the proper implementation and resources is going to be a game changer. 
-      </p>
-      <p className="">
-        For more info, visit About page. 
+        Generates personalized quests according to the context given. 
       </p>
       <p className="py-1">
-        This proof of concept was done with OpenAI API, FastAPI and React.
+        To start testing, create a character.
+      </p>
+      <p className="py-1">
+        For more info, visit About page or <Link to="/about">Click here. </Link>
       </p>
       <div className='pt-10 flex items-baseline'>
         <a href="https://github.com/rodrq" target="_blank" rel="noopener noreferrer"><img className="w-16 h-w-16 mr-6" src={GithubIcon} alt="Github Icon" /></a>
