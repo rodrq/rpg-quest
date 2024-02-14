ccf386c5207133555d9d0bbe8b8da31ab0eefe0d
rodrq <rodrqq@gmail.com>
2024-02-14 02:40:40 -0300

    responsive navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import HomeIconSvg from '../assets/rpg-quest.svg';
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import Modal from './Modal';
-import { useHistory } from 'react-router-dom';
 
-const Navbar = () => {
 
-  const history = useHistory();
+const noAuthNavigation = [
+  { name: 'Home', href: '/'},
+  { name: 'About', href: '/about'},
+  { name: 'Create Character', href: '/register'},
+  { name: 'Login', href: '/login'},
+]
 
+const authNavigation = [
+    { name: 'Home', href: '/'},
+    { name: 'About', href: '/about'},
+    { name: 'Play', href: '/play'},
+    { name: 'Quests', href: '/all_quests'},
+  ]
+  
+
+
+export default function Navbar() {
   const { authenticated, logout } = useAuth();
 
-  const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const history = useHistory()
 
+  const [open, setOpen] = useState(false);
 
   const handleLogout = () => {
     setLogoutModalOpen(true);
   };
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen);
-  };
+  const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
 
   const confirmLogout = () => {
     document.cookie.split(';').forEach((cookie) => {
@@ -39,88 +51,118 @@ const Navbar = () => {
     setLogoutModalOpen(false);
   };
 
+    
   return (
-    <nav className="bg-gray-800 p-4 text-white ">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center font-bold text-lg py-1 pl-5">
-        <div className="lg:hidden">
-          <button
-            onClick={toggleMobileMenu}
-            className="text-white focus:outline-none"
-            aria-label="Toggle Menu"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <Link to="/" className="flex items-center font-bold text-lg py-1 pl-5">
-            <img
-              className="w-10 h-10 mr-2 "
-              src={HomeIconSvg}
-              alt="Home Icon"
-            />
-          </Link>
-          <Link to="/" className="flex items-center  font-bold text-lg py-1 pl-5">
-            Home
-          </Link>
-        </div>
-        <div className="flex items-center font-bold text-lg py-1 pl-5">
-          {authenticated && (
-            <div>
-              <Link to="/play" className="pr-4">
-                Play
-              </Link>
-              <Link to="/all_quests" className="pl-4">
-                Quests
-              </Link>
-            </div>
-          )}
-          <Link to="/about" className="flex items-center  font-bold text-lg py-1 pl-5">
-            About
-          </Link>
-        </div>
-        <div>
-          {authenticated ? (
-            <div className="flex items-center text-lg py-1 pl-5">
-              <div className="">
-                Welcome 
-              </div>
-              <button onClick={handleLogout} className="flex items-center  font-bold text-lg py-1 pl-5">
-                Log out
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <button
+                onClick={() => setOpen(!open)}
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              >
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )}
               </button>
             </div>
-          ) : (
-            <div className="flex items-center  font-bold text-lg py-1 px-5 ">
-              <Link to="/register" >
-                Create Character
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <Link to='/' className="flex flex-shrink-0 items-center">
+                <img
+                  className="h-8 w-auto"
+                  src={HomeIconSvg}
+                  alt="RPG AI quest creator"
+                />
               </Link>
-              <Link to="/login" className="py-1 pl-5 text-yellow-500 hover:text-yellow-300">
-                Log in
-              </Link>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4 items-center">
+                  {authenticated ? (
+                    <>
+                      {authNavigation.map((item) => (
+                        <div key={item.name}>
+                          <Link
+                            to={item.href}
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold"
+                          >
+                            {item.name}
+                          </Link>
+                        </div>
+                      ))}
+                      <div>
+                        <button
+                          onClick={handleLogout}
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold"
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </>
+                  ) : (
+                    noAuthNavigation.map((item) => (
+                      <div key={item.name}>
+                        <Link
+                          to={item.href}
+                          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-lg font-bold"
+                        >
+                          {item.name}
+                        </Link>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </div>
             </div>
-          )}
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 text-gray-100"></div>
+          </div>
         </div>
-      </div>
-      <Modal
-        isOpen={isLogoutModalOpen}
-        onClose={cancelLogout}
-        onConfirm={confirmLogout}
-        message="Are you sure you want to log out?"
-      />
-    </nav>
-  );
-};
-
-export default Navbar;
+        <div className={`sm:hidden ${open ? 'block' : 'hidden'}`}>
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {authenticated ? (
+              <>
+                {authNavigation.map((item) => (
+                  <div key={item.name}>
+                    <Link
+                      to={item.href}
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-bold"
+                    >
+                      {item.name}
+                    </Link>
+                  </div>
+                ))}
+                <div>
+                  <button
+                    onClick={handleLogout}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-bold"
+                  >
+                    Logout
+                  </button>
+                </div>
+              </>
+            ) : (
+              noAuthNavigation.map((item) => (
+                <div key={item.name}>
+                  <Link
+                    to={item.href}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-bold"
+                  >
+                    {item.name}
+                  </Link>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+        <Modal
+          isOpen={isLogoutModalOpen}
+          onClose={cancelLogout}
+          onConfirm={confirmLogout}
+          message="Are you sure you want to log out?"
+        />
+      </nav>
+    );
+  }
