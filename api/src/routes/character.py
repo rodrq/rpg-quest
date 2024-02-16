6c51920b4445fb8455a6383e6cc8380aa9cc9afa
rodrq <rodrqq@gmail.com>
2024-02-16 09:26:35 -0300

    controller to handler

--- api/src/routes/character.py
+++ api/src/routes/character.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from src.models.schemas import CharacterInDb
-from src.controllers.character import create_character_handler
+from src.handlers.character import create_character_handler
 
 
 
