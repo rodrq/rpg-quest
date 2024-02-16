b89d13b5f43f0fb948dfa86318f2cbd1bcacc33f
rodrq <rodrqq@gmail.com>
2024-02-16 10:18:45 -0300

    done config setting for dev and prod same time

--- api/src/config/config.py
+++ api/src/config/config.py
@@ -1,9 +1,12 @@
 import os
+from starlette.config import Config
 
+#First reads env vars, then .env, then default or raises error
+config = Config('.env')
 
-DB_URL = os.getenv('DB_URL')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-OPENAI_ORG_ID = os.getenv('OPENAI_ORG_ID')
-SECRET_KEY=os.getenv('SECRET_KEY')
-ALGORITHM=os.getenv('ALGORITHM')
-TOKEN_LIFETIME_MINUTES=os.getenv('TOKEN_LIFETIME_MINUTES')
+DB_URL = config('DB_URL')
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+OPENAI_ORG_ID = config('OPENAI_ORG_ID')
+SECRET_KEY=config('SECRET_KEY')
+ALGORITHM=config('ALGORITHM')
+TOKEN_LIFETIME_MINUTES=config('TOKEN_LIFETIME_MINUTES')
